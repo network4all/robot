1408bb950cfe92a02c3b331191b516a58ea8bdb9
Arjen <arjen@network4all.nl>
2018-11-20 07:14:01 +0100

    0002

--- main.go
+++ main.go
@@ -27,6 +27,14 @@ type Message struct {
         Ack         bool   `json:"ack"`         // read ack (tcp/udp) (true/false)
 }
 
+const (
+    writeWait = 10 * time.Second        // Time allowed to write a message to the peer.
+    maxMessageSize = 8192               // Maximum message size allowed from peer.
+    pongWait = 60 * time.Second         // Time allowed to read the next pong message from the peer.
+    pingPeriod = (pongWait * 9) / 10    // Send pings to peer with this period. Must be less than pongWait.
+    closeGracePeriod = 10 * time.Second // Time to wait before force close on connection.
+)
+
 func main() {
 	devicename := d.Id()
     // ui
@@ -110,9 +118,8 @@ func main() {
 		ui.Render(p,g)
 	})
 
-
     // keepalive
-    //keepaliveTicker := time.NewTicker(time.Second*15)
+    go ping(c)
 
     // update loop
     drawTicker := time.NewTicker(time.Second)
@@ -230,17 +237,15 @@ func doCommand(msg Message, devicename string, c *websocket.Conn, ) string {
 	return ""
 }
 
-func ping(ws *websocket.Conn, done chan struct{}) {
-    ticker := time.NewTicker(time.Second * 15)
+func ping(ws *websocket.Conn) {
+    ticker := time.NewTicker(pingPeriod)
     defer ticker.Stop()
     for {
         select {
         case <-ticker.C:
             if err := ws.WriteControl(websocket.PingMessage, []byte{}, time.Now().Add(writeWait)); err != nil {
-                log.Println("ping:", err)
+                // log.Println("ping:", err)
             }
-        case <-done:
-            return
         }
     }
 }
