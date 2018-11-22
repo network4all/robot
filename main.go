c4a8b10a253f11a5210e4b85663c5db8ee117497
Arjen <arjen@network4all.nl>
2018-11-22 14:09:27 +0100

    autocommit do 22-11-2018

--- main.go
+++ main.go
@@ -206,7 +206,7 @@ func sendMessageTo (destination string, message string, msgtype int, device stri
 
 func sendPhoto (destination string, device string, c *websocket.Conn) int {
 
-   photo := "/root/scripts/photo/201811221130.jpeg"
+   photo := "/root/scripts/photo/201811221406.jpeg"
 
    // photo := "c:\\temp\\test.jpg"
    encoded := encode(photo)
